e574ebd01d5d3c52c6550c5d521283462e628879
Roshankumar S <2018che0337@svce.ac.in>
2024-01-21 22:08:54 +0530

    Files added

---
+++ Analysis.py.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import os
+
+st.set_page_config(
+    page_title="Job Analysis",
+    page_icon="📈🌐",
+    layout="centered",
+    initial_sidebar_state="collapsed"
+)
+
+files_dict = {}
+for filename in os.listdir("dataset/"):
+    if filename.endswith(".csv"):
+        df = pd.read_csv(os.path.join("dataset/", filename)).iloc[:,1:]
+        files_dict[filename[:-4]] = df
+
+selected_df = st.multiselect("Select the datasets to analyse:",files_dict.keys())
+
+dfs = []
+
+for df in selected_df:
+    dfs.append(files_dict[df])
+
+if "analyze" not in st.session_state:
+    st.session_state["analyze"]=False
+
+if len(dfs)!=0:
+    df = pd.concat(dfs,ignore_index=True)
+    df.index = list(range(1,len(df)+1))
+    st.dataframe(df)
+    st.session_state["analyze"] = st.button("Analyze Job Titles!")
+
+if st.session_state["analyze"]:
+    st.subheader("Words Frequency")
+    words = []
+    for index in df.index:
+        words.append(df.loc[index,"jobTitle"].split())
+    words = [i for j in words for i in j]
+    words = [i.lower() for i in words]
+    st.dataframe(pd.DataFrame(pd.Series(words).value_counts()).rename(columns={0:"Count"}))
+    
+
+    
---
+++ pages/Web Scrapper.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import requests
+import json
+from stqdm import stqdm
+import pandas as pd
+
+st.set_page_config(
+    page_title="Web Scrapping",
+    page_icon="📈🌐",
+    layout="centered",
+    initial_sidebar_state="collapsed"
+)
+
+url = "https://api.scrapingdog.com/indeed"
+api_key = "65a538d6446d2a02612a5980"
+
+if "Scrape" not in st.session_state:
+    st.session_state["Scrape"] = False
+
+if "Save_Button" not in st.session_state:
+    st.session_state["Save_Button"] = False
+
+st.subheader("Click the button to scrape today's jobs from indeed")
+st.session_state["Scrape"] = st.button("Scrape!")   
+
+if st.session_state["Scrape"]:
+    jobs_list = []
+    for i in stqdm(range(1,54)):
+        # job_search_url = f"https://www.indeed.com/jobs?q=CDL A Drivers&l=United States&start={i*10}&vjk=8bf2e735050604df"
+        job_search_url = f"https://www.indeed.com/jobs?q=CDL+A+Drivers&l=United+States&sc=0kf%3Aattr%28VU74M%29%3B&radius=50&start={i*10}&vjk=b45f8bcb33974bc3"
+
+        # Set up the parameters
+        params = {"api_key": api_key, "url": job_search_url}
+        # Make the HTTP GET request
+        response = requests.get(url, params=params)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON content
+            json_response = response.json()
+            jobs_list.append(json_response)
+        else:
+            print(f"Error: {response.status_code}")
+            print(response.text)
+    jobs_data = pd.DataFrame([i for j in jobs_list for i in j])
+    jobs_data.drop(["totalJobs","jobDescription"],axis=1,inplace=True)
+    st.session_state["Save_Button"] = True
+
+if st.session_state["Save_Button"]:
+    st.dataframe(jobs_data)
+    st.subheader("Do you want to save the data ?")
+    st.download_button("Download Output! (PDF)",jobs_data.to_csv(index=False).encode('utf-8'),"Jobs List (21-01-2024).csv","text/csv")

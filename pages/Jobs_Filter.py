3c684ce21f655308043c35df0c1593ba9432f829
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-23 21:47:58 +0530

    Update Jobs_Filter.py

--- pages/Jobs_Filter.py
+++ pages/Jobs_Filter.py
@@ -8,7 +8,7 @@ for filename in os.listdir("Dataset/"):
         df = pd.read_csv(os.path.join("Dataset/", filename)).iloc[:,1:]
         files_dict[filename[:-4]] = df
 
-selected_df = st.multiselect("Select the datasets to analyse:",files_dict.keys(),default=[-1])
+selected_df = st.multiselect("Select the datasets to analyse:",files_dict.keys(),default=[0])
 
 dfs = []
 

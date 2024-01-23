76330d6c71f62a4af698a647bf82ed8e423b19d2
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-23 22:24:17 +0530

    Update Jobs_Filter.py

--- pages/Jobs_Filter.py
+++ pages/Jobs_Filter.py
@@ -32,7 +32,7 @@ location = st.multiselect("State:",["All"]+list(df["companyLocation"].unique()))
 st.session_state["Search"] = st.button("Search!")
 
 if st.session_state["Search"]:
-    df.drop(df['jobTitle'].isnull().index,inplace=True)
+    
     if not location or "All" in location:
         filtered_df = df[df['jobTitle'].str.contains(keyword, case=False)]
     else:

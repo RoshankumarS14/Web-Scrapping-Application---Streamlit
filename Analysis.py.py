c42e3a8bf2a73b8414df3fe65322dd46896d4eed
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-23 21:15:25 +0530

    Update Analysis.py.py

--- Analysis.py.py
+++ Analysis.py.py
@@ -11,16 +11,16 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-st.markdown(
-    """
-<style>
-    [data-testid="collapsedControl"] {
-        display: none
-    }
-</style>
-""",
-    unsafe_allow_html=True,
-)
+# st.markdown(
+#     """
+# <style>
+#     [data-testid="collapsedControl"] {
+#         display: none
+#     }
+# </style>
+# """,
+#     unsafe_allow_html=True,
+# )
 
 # Define your pages
 PAGES = {

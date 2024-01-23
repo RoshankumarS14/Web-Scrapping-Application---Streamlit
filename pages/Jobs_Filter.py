4d4d70369394e3ba3105596c6fe60fa049cc4a12
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-23 22:23:04 +0530

    Update Jobs_Filter.py

--- pages/Jobs_Filter.py
+++ pages/Jobs_Filter.py
@@ -6,6 +6,10 @@ files_dict = {}
 for filename in os.listdir("Dataset/"):
     if filename.endswith(".csv"):
         df = pd.read_csv(os.path.join("Dataset/", filename)).iloc[:,:]
+        try:
+            df.drop(["Unnamed: 0"],axis=1,inplace=True)
+        except:
+            pass
         files_dict[filename[:-4]] = df
 
 selected_df = st.multiselect("Select the datasets to analyse:",["All"]+list(files_dict.keys()),default=["All"])

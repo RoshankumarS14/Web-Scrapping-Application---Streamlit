e333ed5115d17e241475fdc7c15fae41d09ead8a
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-23 22:43:22 +0530

    Update Analysis.py.py

--- Analysis.py.py
+++ Analysis.py.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import os
 from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
@@ -39,7 +40,31 @@ if "selected_page" not in st.session_state:
 #     if st.session_state["selected_page"] != "Analysis":
 #         switch_page(PAGES[st.session_state["selected_page"]])
 #     st.rerun()
-        
+
+conversion_factor = {"hour":8760,"day":365,"week":52,"month":12,"year":1}
+def extract_salary(salary):
+    salary = salary.lower()
+    if salary.count("hour")>0:
+        timeframe="hour"
+    elif salary.count("day")>0:
+        timeframe="day"
+    elif salary.count("week")>0:
+        timeframe="week"
+    elif salary.count("month")>0:
+        timeframe="month"
+    elif salary.count("year")>0:
+        timeframe="year"
+    # else:
+    #     return np.nan
+    salary = salary.replace(" ","")
+    salary = salary.replace("$","")
+    salary = salary.replace(",","")
+    number = "".join([i for i in salary if not i.isalpha()])
+    if number.count("-")>0:
+        range = number.split("-")
+        number = (float(range[0])+float(range[1]))/2
+    return float(number)*conversion_factor[timeframe]
+    
 files_dict = {}
 for filename in os.listdir("Dataset/"):
     if filename.endswith(".csv"):
@@ -58,6 +83,7 @@ if "analyze" not in st.session_state:
 
 if len(dfs)!=0:
     df = pd.concat(dfs,ignore_index=True)
+    df["Salary (yearly)] = df["Salary"].apply(lambda a : extract_salary(a) if a!="-" else np.nan)
     df.index = list(range(1,len(df)+1))
     st.dataframe(df)
     st.session_state["analyze"] = st.button("Analyze Job Titles!")

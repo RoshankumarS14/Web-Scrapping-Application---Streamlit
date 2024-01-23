9d07405b5bcd1f9786df390ca6f26168d23df338
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-23 21:09:50 +0530

    Update Analysis.py.py

--- Analysis.py.py
+++ Analysis.py.py
@@ -11,20 +11,20 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# st.markdown(
-#     """
-# <style>
-#     [data-testid="collapsedControl"] {
-#         display: none
-#     }
-# </style>
-# """,
-#     unsafe_allow_html=True,
-# )
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
 # Define your pages
 PAGES = {
-    "Analysis": "streamlit app",
+    "Analysis": "analysis.py",
     "Scrap Jobs": "web scrapper"
 }
 

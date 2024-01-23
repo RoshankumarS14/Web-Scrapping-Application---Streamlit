205c36aacede445f328966c8dc51c62dd8c16d14
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-23 21:17:26 +0530

    Update Jobs_Filter.py

--- pages/Jobs_Filter.py
+++ pages/Jobs_Filter.py
@@ -6,6 +6,9 @@ jobs = ['Software Engineer', 'Data Scientist', 'Product Manager', 'UX Designer',
 # Search box
 keyword = st.text_input('Enter a keyword to search for jobs')
 
+df_html = state_wise_pop.to_html(classes='table table-striped',index=False)
+df_html = df_html.replace('<table ','<table style="text-align:right; margin-bottom:40px; margin-top:50px; width:95%;" ')
+
 # Filter jobs
 filtered_jobs = [job for job in jobs if keyword.lower() in job.lower()]
 

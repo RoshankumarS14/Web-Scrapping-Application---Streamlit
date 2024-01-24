58cda292c984b4d03ea20dd03cff8b39f2f52242
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-24 22:49:54 +0530

    Update Analysis.py.py

--- Analysis.py.py
+++ Analysis.py.py
@@ -69,7 +69,10 @@ files_dict = {}
 for filename in os.listdir("Dataset/"):
     if filename.endswith(".csv"):
         df = pd.read_csv(os.path.join("Dataset/", filename))
-        # df.drop(["Unnamed:0"],axis=1,inplace=True)
+        try:
+            df.drop(["Unnamed: 0"],axis=1,inplace=True)
+        except:
+            pass
         files_dict[filename[:-4]] = df
 
 selected_df = st.multiselect("Select the datasets to analyse:",files_dict.keys())

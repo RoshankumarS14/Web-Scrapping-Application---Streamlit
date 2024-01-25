6d32624e823914e476faa5761d547bc6995c2dcd
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-25 22:55:21 +0530

    Update Meta_Data.py

--- pages/Meta_Data.py
+++ pages/Meta_Data.py
@@ -19,5 +19,5 @@ benefits = df[df["jobTitle"]==st.session_state["job"]]["Benefits (Metadata)"].va
 st.markdown(benefits,unsafe_allow_html=True)
 
 st.subheader("Description")
-description = df[df["jobTitle"]==st.session_state["job"]]["Description (Metadata)"].values[0].replace('\n', '<br>')
+description = df[df["jobTitle"]==st.session_state["job"]]["Description (Metadata)"].values[0].replace('\n', '<br>').replace("$","")
 st.markdown(description,unsafe_allow_html=True)

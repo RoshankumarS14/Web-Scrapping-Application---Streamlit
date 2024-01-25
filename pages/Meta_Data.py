0a6ac5c381d93e59f88a6c20a515b27409d2cda2
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-25 22:45:22 +0530

    Create Meta_Data.py

---
+++ pages/Meta_Data.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("Sample (Meta Data).csv")
+
+if "job" not in st.session_state:
+    st.session_state["job"] = ""
+
+st.session_state["job"] = st.select_box("Select the Job Title:",df["jobTitle"])
+
+st.subheader("Job Title")
+st.write(st.session_state["job"])
+
+st.subheader("Salary")
+st.write(df[df["jobTitle"]==st.session_state["job"]]["Salary (Metadata)"].values[0])
+
+st.subheader("Benefits")
+st.write(df[df["jobTitle"]==st.session_state["job"]]["Benefits (Metadata)"].values[0])
+
+st.subheader("Description")
+st.write(df[df["jobTitle"]==st.session_state["job"]]["Description  (Metadata)"].values[0])

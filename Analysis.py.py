b3646d4ecc317a224a495285adc95c810be0d91b
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-30 22:45:45 +0530

    Update Analysis.py.py

--- Analysis.py.py
+++ Analysis.py.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
+import asana
+import requests
+from io import StringIO
 
 st.set_page_config(
     page_title="Job Analysis",
@@ -12,75 +14,43 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# st.markdown(
-#     """
-# <style>
-#     [data-testid="collapsedControl"] {
-#         display: none
-#     }
-# </style>
-# """,
-#     unsafe_allow_html=True,
-# )
-
-# Define your pages
-PAGES = {
-    "Analysis": "analysis.py",
-    "Scrap Jobs": "web scrapper"
-}
+personal_access_token = '2/1206474259581486/1206474150741575:63af673ccea40cb54a89691b17afc264'
+section_id = '1206473999726567'
+client = asana.Client.access_token(personal_access_token)
 
 if "selected_page" not in st.session_state:
     st.session_state["selected_page"]=False
-
-# # Create an option menu in the main section
-# st.session_state["selected_page"] = option_menu("", list(PAGES.keys()),icons=["list-task"],
-#     menu_icon="cast", default_index=0, orientation="horizontal")
-
-# if st.session_state["selected_page"]:
-#     if st.session_state["selected_page"] != "Analysis":
-#         switch_page(PAGES[st.session_state["selected_page"]])
-#     st.rerun()
-
-conversion_factor = {"hour":8760,"day":365,"week":52,"month":12,"year":1}
-def extract_salary(salary):
-    salary = salary.lower()
-    if salary.count("hour")>0:
-        timeframe="hour"
-    elif salary.count("day")>0:
-        timeframe="day"
-    elif salary.count("week")>0:
-        timeframe="week"
-    elif salary.count("month")>0:
-        timeframe="month"
-    elif salary.count("year")>0:
-        timeframe="year"
-    else:
-        return np.nan
-    salary = salary.replace(" ","")
-    salary = salary.replace("$","")
-    salary = salary.replace(",","")
-    number = "".join([i for i in salary if not i.isalpha()])
-    if number.count("-")>0:
-        range = number.split("-")
-        number = (float(range[0])+float(range[1]))/2
-    return float(number)*conversion_factor[timeframe]
     
-files_dict = {}
-for filename in os.listdir("Dataset/"):
-    if filename.endswith(".csv"):
-        df = pd.read_csv(os.path.join("Dataset/", filename))
-        try:
-            df.drop(["Unnamed: 0"],axis=1,inplace=True)
-        except:
-            pass
-        files_dict[filename[:-4]] = df
+if "jobs_data" not in st.session_state:
+    tasks = client.tasks.get_tasks_for_section(section_gid=section_id, opt_fields='name')
+    jobs_data = {}  # Dictionary to store DataFrames
 
-selected_df = st.multiselect("Select the datasets to analyse:",files_dict.keys())
+    for task in tasks:
+        # Retrieve attachments for each task
+        attachments = client.attachments.find_by_task(task['gid'], opt_fields='name')
+
+        for attachment in attachments:
+            # Check if the attachment is a CSV file
+            if attachment['name'].endswith('.csv'):
+                # Request the specific attachment by its gid to get the download URL
+                attachment_details = client.attachments.get_attachment(attachment['gid'], opt_fields='download_url')
+                download_url = attachment_details['download_url']
+
+                # Download the CSV content
+                response = requests.get(download_url)
+                if response.status_code == 200:
+                    # Read the content into a DataFrame
+                    csv_string = StringIO(response.content.decode('utf-8'))
+                    df = pd.read_csv(csv_string)
+                    jobs_data[attachment['name'].replace(".csv","")] = df
+    st.session_state["jobs_data"]=jobs_data
+
+selected_df = st.multiselect("Select the datasets to analyse:",st.session_state["jobs_data"].keys())
 
 dfs = []
 
 for df in selected_df:
-    dfs.append(files_dict[df])
+    dfs.append(st.session_state["jobs_data"][df])
 
 if "analyze" not in st.session_state:
     st.session_state["analyze"]=False
@@ -94,20 +64,13 @@ if len(dfs)!=0:
     st.session_state["analyze"] = st.button("Analyze Job Titles!")
 
 if st.session_state["analyze"]:
-    c1,c2 = st.columns([1,1])
-    with c1:
-        st.subheader("Words Frequency")
-        words = []
-        for index in df.index:
-            words.append(df.loc[index,"jobTitle"].split())
-        words = [i for j in words for i in j]
-        words = [i.lower() for i in words]
-        st.dataframe(pd.DataFrame(pd.Series(words).value_counts()).rename(columns={0:"Count"}))
-    with c2:
-        st.subheader("Salary trend")
-        date = [i[11:-1] for i in list(files_dict.keys())]
-        mean_salary = [df["Salary"].dropna().apply(lambda a : extract_salary(a) if a!="-" else np.nan).mean() for df in list(files_dict.values())]
-        st.dataframe(pd.DataFrame({"Date":date,"Average Yearly Salary":mean_salary}))
+    st.subheader("Words Frequency")
+    words = []
+    for index in df.index:
+        words.append(df.loc[index,"jobTitle"].split())
+    words = [i for j in words for i in j]
+    words = [i.lower() for i in words]
+    st.dataframe(pd.DataFrame(pd.Series(words).value_counts()).rename(columns={0:"Count"}))
     
 
     
